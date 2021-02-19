068584058b9150d956ad72199ac082679e5d41a9
Jefferson Brandão <jeffersonvideo125@gmail.com>
2021-02-19 18:57:50 -0300

    Added services and interface implementation

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using Course.Entities;
+using Course.Services;
 
 namespace Course
 {
@@ -15,8 +16,19 @@ namespace Course
             DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
             Console.Write("Contract value: ");
             double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
 
-            Contract contract = new Contract(number, date, contractValue);
+            Contract myContract = new Contract(number, date, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installments installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
 
 
 
